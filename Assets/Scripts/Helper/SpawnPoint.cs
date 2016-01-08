eaec1f255c9edbfd02e3a4388300046deb00dfae
Carolin Deeg <carolin.deeg@gmail.com>
2016-01-08 17:39:20 +0100

    cleanup

---
+++ Assets/Scripts/Helper/SpawnPoint.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnPoint : MonoBehaviour
+{
+
+	public GameObject spawnThis;
+	public Transform spawnHere;
+
+	void Start ()
+	{
+		Instantiate( spawnThis, spawnHere.position, Quaternion.identity );
+	}
+}
