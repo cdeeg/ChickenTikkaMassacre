49da11547fef2449b2c2829c40c5f83be021adea
Alise11 <blackangel12321@hotmail.com>
2016-01-31 00:51:02 +0100

    Added new prefabs and began building game in StartGameScene

---
+++ Assets/Scripts/UV_Scrolling.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UV_Scrolling : MonoBehaviour {
+
+	public Material sharedMat;
+
+	//scroll main texture based on time
+
+	public float scrollSpeed = 0.5f;
+	float offset;
+	//float rotate;
+
+	//Renderer mRenderer;
+	
+	void Update ()
+	{
+		if (sharedMat != null) {
+			offset += (Time.deltaTime * scrollSpeed) / 12.0f;
+			sharedMat.SetTextureOffset ("_MainTex", new Vector2 (0, offset));
+		}
+	} 
+
+}
