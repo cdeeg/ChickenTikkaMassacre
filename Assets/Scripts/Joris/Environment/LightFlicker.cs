c33d35ac967a2359a67bc8001fdc15cfaa37e950
joris drobka <joris.drobka@gmx.de>
2016-01-31 15:05:38 +0100

    level + character controller Cameraeffects Vegetation

---
+++ Assets/Scripts/Joris/Environment/LightFlicker.cs
@@ -0,0 +1,39 @@
+﻿//=================================================================================================================
+
+using UnityEngine;
+using System.Collections;
+
+//=================================================================================================================
+
+[RequireComponent(typeof(Light))]
+public class LightFlicker : MonoBehaviour
+{
+
+	public float xamp;
+	public float yamp;
+	public float zamp;
+	public float xfreq;
+	public float yfreq;
+	public float zfreq;
+
+	//new Light light;
+	Vector3 startPosLocal;
+
+	void Start () 
+	{
+		//light 			= GetComponent<Light>();
+		startPosLocal 	= transform.localPosition;
+	}
+	
+	void Update()
+	{
+
+		float x = Mathf.Sin(xfreq * Time.time) * xamp;
+		float y = Mathf.Sin(yfreq * Time.time) * yamp;
+		float z = Mathf.Cos(zfreq * Time.time) * zamp;
+
+		transform.localPosition = startPosLocal + new Vector3(x,y,z);
+	}
+}
+
+//=================================================================================================================
---
+++ Assets/Scripts/Joris/Environment/VegetationRustle.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(SphereCollider), typeof(Animation))]
+public class VegetationRustle : MonoBehaviour
+{
+	Animation anim;
+
+	void Awake()
+	{
+		anim = GetComponent<Animation>();
+	}
+
+	void OnTriggerStay(Collider other)
+	{
+		Rigidbody r = other.GetComponent<Rigidbody>();
+
+		Debug.Log("r velocity: " + r.velocity.magnitude);
+		if(r.velocity.magnitude > 0.2f && !anim.isPlaying)
+		{
+			anim.Play();
+		}
+	}
+}
